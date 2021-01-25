278e04534dbef2f59e09bd2ad944f90e579d1aaf
Andrew Carbert <ac.believer@gmail.com>
2021-01-24 20:59:19 -0600

    update

--- key-moments.py
+++ key-moments.py
@@ -7,7 +7,7 @@ import pyperclip as clipboard
 from win10toast import ToastNotifier
 toaster = ToastNotifier()
 
-version="2.4"
+version="2.5"
 
 OUTPUT_OPTION_CLIPBOARD = "Copy To Clipboard"
 OUTPUT_OPTION_CONSOLE = "Console"
